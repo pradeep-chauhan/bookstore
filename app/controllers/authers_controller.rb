00bb5e7a6e6d06ef90f6f15e8d059a51a911cb35
pradeep-chauhan <prdpchauhan4@gmail.com>
2023-03-06 18:54:01 +0530

    added contoller actions and renaimend filename

---
+++ app/controllers/authers_controller.rb
@@ -0,0 +1,41 @@
+class AuthersController < ApplicationController
+
+  def index
+    render json: { authers: Auther.all }, status: :ok
+  end
+
+  def show
+    render json: { auther: @auther }, status: :ok
+  end
+
+  def create
+    @auther = auther.new(auther_params)
+    @auther.save!
+    render json: { auther: @auther }, status: :ok
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def update
+    @auther.update!(auther_params)
+    render json: { auther: @auther }, status: :ok
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def destroy
+    @auther.destroy!
+    render json: {}, status: :ok
+  end
+
+  private
+
+  def exists
+    @auther = Auther.find_by_id(params[:id])
+    render json: { error: 'Auther is not found' }, status: :not_found unless @auther.present?
+  end
+
+  def auther_params
+    params.require(:auther).permit(:name)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_115735) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_123624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_115735) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["auther_id"], name: "index_books_on_auther_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "books", "authers"
