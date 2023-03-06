aaaca3fc3fcd1f752881ef37cd610e2217116cf8
pradeep-chauhan <prdpchauhan4@gmail.com>
2023-03-06 19:01:53 +0530

    added price for book

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 auther = Auther.find_or_create_by(name: 'John D.')
-Book.find_or_create_by(title: "Shape of mind", isbn: "12345qwerty", quantity: 10, auther: auther)
+Book.find_or_create_by(title: "Shape of mind", isbn: "12345qwerty", quantity: 10, price: 100.0, auther: auther)
