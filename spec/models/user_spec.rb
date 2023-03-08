4bba2603dd4b9c1f65727820d9e0a5801f1503fc
pradeep-chauhan <prdpchauhan4@gmail.com>
2023-03-09 01:15:47 +0530

    updated user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe User, :type => :model do
     it "is valid with valid attributes" do
       expect(subject).to be_valid
     end
+
+    it 'should have a name' do
+      expect(subject.name).to be_present
+    end
+
+    it 'should have a email' do
+      expect(subject.email).to be_present
+    end
+
+    it 'should have a password' do
+      expect(subject.password).to be_present
+    end
   end
 end
