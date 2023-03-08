42a96c197e0c93c4ae136e4e2c4c58f7ec2cabdf
pradeep-chauhan <prdpchauhan4@gmail.com>
2023-03-09 00:46:55 +0530

    added spec for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  
+  let(:user) { create(:user) }
+  subject { build(:user) }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+  end
 end
