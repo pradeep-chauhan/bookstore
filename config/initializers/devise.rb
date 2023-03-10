37efe7aea69a329269cd8f953421d6d8b24015c1
pradeep-chauhan <prdpchauhan4@gmail.com>
2023-03-10 11:02:51 +0530

    updated circleci config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'c1ffdcfaf48588e0c74e74c161ca8c4051daad3790fb366a492fbd7dbb890cb083a447278c2b7bababf17476e14ec2171d4d19c8f23b21878eea6e96d5571e29'
+  config.secret_key = "#{ENV['DEVISE_SECRET_KEY']}"
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
